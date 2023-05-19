0738b27138d8d484be7270b15d3315f9783a27d9
reinisrm <rreeiinniiss@gmail.com>
2023-05-19 20:45:52 +0300

    Pievienots sākuma skats

---
+++ src/resources/views/home/index.blade.php
@@ -0,0 +1,10 @@
+
+@extends('layout')
+
+@section('content')
+
+    
+    <h1>PD2 - {{ $title }}</h1>
+
+
+@endsection
---
+++ src/resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html class="no-js" lang="lv">
+
+<head>
+    <meta charset="utf-8">
+    <title>PD2 - {{ $title }}</title>
+    <meta name="description" content="Tīmekļu Tehnoloģiju 2. praktiskais darbs">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+    @yield('content')
+
+</body>
+
+</html>
