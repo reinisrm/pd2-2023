878eb5e7d5415d38fd633eb5d45f60badea71c86
reinisrm <rreeiinniiss@gmail.com>
2023-05-20 18:29:39 +0300

    Iesakta autoru sadala

---
+++ src/resources/views/author/list.blade.php
@@ -0,0 +1,19 @@
+
+@extends('layout')
+
+@section('content')
+
+    @if (count($items) > 0)
+
+    //
+    
+    @else
+
+        <p>Nav atrasts neviens ieraksts</p>
+
+    @endif
+
+
+
+
+@endsection
